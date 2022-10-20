12ada207e17fc6d310363b05c320fd4534497b5c
Danilo Silva <danilo.silva@casavo.com>
2022-10-20 15:02:05 +0000

    chore: improve

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ try {
     .then((res) => {
       const successful_commits = res.data.workflow_runs.map(run => run.head_commit.id)
 
-      let xxx = JSON.stringify(successful_commits).substring(0, 2048)
-      core.setFailed(`Error ${xxx}, action may still succeed though`);
+      const has_already_ran = successful_commits.includes(core.getInput("commit_sha"))
 
-      // const lastSuccessCommitHash =
-      //   res.data.workflow_runs.length > 0
-      //     ? res.data.workflow_runs[0].head_commit.id
-      //     : "";
-      core.setOutput("has_already_ran", true);
+      if (has_already_ran) {
+        core.info(`A successful workflow run for commit ${core.getInput("commit_sha")} was found`)
+      } else {
+        core.info(`No successful workflow run for commit ${core.getInput("commit_sha")} was found`)
+      }
+
+      core.setOutput("has_already_ran", has_already_ran);
     })
     .catch((e) => {
       core.setFailed(e.message);
